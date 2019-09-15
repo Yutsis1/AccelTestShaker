21ccc28937cb7ac8befcc8a07e51166193ec8e3d
Roman Yutsis <yucis@author.spb.ru>
2019-09-15 14:22:34 +0300

    make new functions for accelerometer

--- app/src/main/java/com/example/acceltest/MainActivity.kt
+++ app/src/main/java/com/example/acceltest/MainActivity.kt
@@ -7,25 +7,88 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
+
+//    values for Sensor
+    private lateinit var senSensorManager: SensorManager
+    private lateinit var senAccelerometer: Sensor
+//    Values for work sensors
+    private var lastUpdate: Long = 0
+    private var last_X: Float = 0f
+    private var last_Y: Float = 0f
+    private var last_Z: Float = 0f
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 //        make sensors values
-        val senSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        val senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        senSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
-    override fun onSensorChanged(p0: SensorEvent?) {
+    override fun onSensorChanged(sensorEvent: SensorEvent?) {
+        val mySensor = sensorEvent?.sensor
+
+        if (mySensor!!.type == Sensor.TYPE_ACCELEROMETER){
+
+            val x = sensorEvent.values[0]
+            val y = sensorEvent.values[1]
+            val z = sensorEvent.values[2]
+
+            var currentTime: Long = System.currentTimeMillis()
+
+            if ((currentTime - lastUpdate)>100){
+                val difTime = (currentTime-lastUpdate)
+                lastUpdate = currentTime
+                val speed:Float = Math.abs(x+y+z - last_X -last_Y - last_Z)/difTime*10_000
+                if (speed > SHAKE_THRESHOLD){
+
+                }
+                last_X=x
+                last_Y=y
+                last_Z=z
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(Sensor: Sensor?, accuary: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//    functions for make new
+    override fun onPause(){
+        super.onPause()
+        senSensorManager.unregisterListener(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+//    spectial functions
+    fun getRandomValues(){
+        var generetadNumbers:ArrayList<Int> = arrayListOf(6)
+        var cnt:Int = 0
+        while (cnt < 6){
+            val rnd = Random.nextInt(48)+1
+            if (!generetadNumbers.contains(rnd)){
+                generetadNumbers.add(rnd)
+            } else {
+                cnt--
+            }
+        }
+
+    }
+
+    companion object{
+        val SHAKE_THRESHOLD = 600
     }
 
 }
